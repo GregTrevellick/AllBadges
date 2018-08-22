2e03a1193725e169b6502031c0e88d0eaab119ad
Greg Trevellick <gtrevellick@googlemail.com>
2018-08-22 16:15:29 +0100

    .

---
+++ Src/DummyProject/DummyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyProject
+{
+    class DummyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
